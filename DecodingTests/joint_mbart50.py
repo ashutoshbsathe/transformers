39e046b35538bd3ff83f13c16dc66ab3a6882f0b
Ashutosh Sathe <2ashutoshbs@gmail.com>
2022-07-14 12:51:00 +0530

    Towards joint mBART thingy

--- DecodingTests/joint_mbart50.py
+++ DecodingTests/joint_mbart50.py
@@ -1,8 +1,10 @@
 # Run as follows 
 # `time TRANSFORMERS_OFFLINE=1 PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python python joint_mbart50.py`
 # This is slow AF because we use python protobuf instead of C++ one but it works reliabily on every system 
+import torch
 from transformers import MBartForConditionalGeneration, MBart50TokenizerFast
-
+from new_from_hf import beam_search
+from copy import deepcopy 
 article_hi = "संयुक्त राष्ट्र के प्रमुख का कहना है कि सीरिया में कोई सैन्य समाधान नहीं है"
 article_ar = "الأمين العام للأمم المتحدة يقول إنه لا يوجد حل عسكري في سوريا."
 
@@ -19,23 +21,44 @@ gen_args = dict(
     return_args=True,
 )
 
-# translate Hindi to English
-tokenizer.src_lang = "hi_IN"
-encoded_hi = tokenizer(article_hi, return_tensors="pt")
-generated_tokens, hi_args = model.generate(
-    **encoded_hi,
-    **gen_args,
-)
-print('\n'.join(tokenizer.batch_decode(generated_tokens['sequences'], skip_special_tokens=True)))
-# => "Le chef de l 'ONU affirme qu 'il n 'y a pas de solution militaire dans la Syrie."
+with torch.no_grad():
 
-# translate Arabic to English
-tokenizer.src_lang = "ar_AR"
-encoded_ar = tokenizer(article_ar, return_tensors="pt")
-generated_tokens, ar_args = model.generate(
-    **encoded_ar,
-    **gen_args,
-)
-print('\n'.join(tokenizer.batch_decode(generated_tokens['sequences'], skip_special_tokens=True)))
-# => "The Secretary-General of the United Nations says there is no military solution in Syria."
+    # Standalone Hindi --> English 
+    tokenizer.src_lang = "hi_IN"
+    encoded_hi = tokenizer(article_hi, return_tensors="pt")
+    out1, hi_args = model.generate(
+        **encoded_hi,
+        **gen_args,
+    )
+    print('Standalone Hindi --> English [HF generate]')
+    print(f'Input: ""{article_hi}""\nOutput:')
+    for i, seq in enumerate(tokenizer.batch_decode(out1['sequences'], skip_special_tokens=True)):
+        print(f'{i+1} {out1["sequences_scores"][i].item():.3f} {seq}')
+    
+    hi_model_kwargs = hi_args.pop('model_kwargs')
+    out2 = beam_search(model, **deepcopy(hi_args), **deepcopy(hi_model_kwargs))
+    print('Standalone Hindi --> English [Our beam_search]')
+    print(f'Input: ""{article_hi}""\nOutput:')
+    for i, seq in enumerate(tokenizer.batch_decode(out2['sequences'], skip_special_tokens=True)):
+        print(f'{i+1} {out2["sequences_scores"][i].item():.3f} {seq}')
+
+    # Standalone Arabic --> English
+    tokenizer.src_lang = "ar_AR"
+    encoded_ar = tokenizer(article_ar, return_tensors="pt")
+    out3, ar_args = model.generate(
+        **encoded_ar,
+        **gen_args,
+    )
+    print('Standalone Arabic --> English [HF generate]')
+    print(f'Input: ""{article_ar}""\nOutput:')
+    for i, seq in enumerate(tokenizer.batch_decode(out3['sequences'], skip_special_tokens=True)):
+        print(f'{i+1} {out3["sequences_scores"][i].item():.3f} {seq}')
+    
+    ar_model_kwargs = ar_args.pop('model_kwargs')
+    out4 = beam_search(model, **deepcopy(ar_args), **deepcopy(ar_model_kwargs))
+    print('Standalone Arabic --> English [Our beam_search]')
+    print(f'Input: ""{article_ar}""\nOutput:')
+    for i, seq in enumerate(tokenizer.batch_decode(out4['sequences'], skip_special_tokens=True)):
+        print(f'{i+1} {out4["sequences_scores"][i].item():.3f} {seq}')
+
 
